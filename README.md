7417e792384f95a6bcbaed76946f3160d8f39706
gilfoyle97 <edoardott@gmail.com>
2021-02-02 18:59:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Based on [m4ll0k/Secret-Finder](https://github.com/m4ll0k/SecretFinder)
+
+Basically it runs Secret-Finder on multiple urls, edit the last line of
+`auto-secret-finder.py` as you want to fit your goals.
+
+### Installation
+
+```
+$ git clone https://github.com/Fricciolosa-Red-Team/AutoSecretFinder.git autosecretfinder
+$ cd autosecretfinder
+$ python -m pip install -r requirements.txt or pip install -r requirements.txt
+$ python3 auto-secret-finder.py subs.txt
+```
+where subs.txt is a list of domains.
---
+++ auto-secret-finder.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import requests
+file1 = open(sys.argv[1], 'r')
+Lines = file1.readlines()
+
+def url_ok(url):
+    try:
+        r = requests.get(url,timeout=5)
+        if r:
+            return True
+        return False
+    except Exception as e:
+        return False
+
+
+path = "auto-secret-finder"
+try:
+    os.mkdir(path)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+
+count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("[ * ] Scanning {}...".format(count))
+    if url_ok("http://{}".format(line.strip())):
+        os.system("python3 SecretFinder.py -i http://{} -o auto-secret-finder/{}.html".format(line.strip(), line.strip()))
+
+# EDIT THIS LAST LINE AS YOU WANT TO FIT YOUR GOALS!
